d485c76205c0c2f8098409b85439db5a755e8622
B SAI GANESH <149352630+ganesh75690@users.noreply.github.com>
2025-04-28 12:43:56 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,17 @@
+# a program to perform basic arithmetic operations
+a = 20
+
+b = 45
+add = a+b
+sub = a-b
+multi = a*b
+div = a / b
+mod = a%b
+
+print("sum is",add)
+print("sub value is",sub)
+print("multi is ",multi)
+print("divison value is ",div)
+print("mod of values is",mod)
+
+print("here all values are solved") 
