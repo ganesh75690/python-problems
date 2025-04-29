29c56bef9f5975cc60098225a022814e0ebba8da
B SAI GANESH <149352630+ganesh75690@users.noreply.github.com>
2025-04-29 17:43:19 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,15 @@
+
+#program to find simple interest
+
+def simple_interest(p, r, t):
+    # Simple Interest = (Principal * Rate * Time) / 100
+    return (p * r * t) / 100
+
+p,r,t = 2 ,6,4
+
+res=simple_interest(p,r,t)
+
+print("S I os ",res)                   
+                                         
+
+
