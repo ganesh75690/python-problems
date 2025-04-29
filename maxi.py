5648d1c2682298ee1ee898c3e6dbe655562daa58
B SAI GANESH <149352630+ganesh75690@users.noreply.github.com>
2025-04-29 10:56:36 +0530

    Add files via upload

---
+++ maxi.py
@@ -0,0 +1,19 @@
+#program to find maximum of two numbers
+
+x = 34
+
+y = 66
+
+print(max(x ,y))
+
+print(min(x, y))
+#program to find minimum of two numbers
+
+print(abs(x-y))
+
+print(x+y)
+print(x-y) 
+
+# abs = absolute value = difference between two numbers 
+# max = maximum value = greater of two numbers 
+# min = minimum value = smaller of two numbers
