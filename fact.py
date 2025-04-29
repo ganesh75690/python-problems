0615bce573fe870b975bbc52855dc59a61ae14ce
B SAI GANESH <149352630+ganesh75690@users.noreply.github.com>
2025-04-29 11:36:09 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,18 @@
+
+#program to get factorial of a number using math module
+import math
+n = 5
+ # # factorial = product of all positive integers from 1 to n
+ # # # factorial of 0 = 1
+    # # # factorial of 1 = 1
+    # # # factorial of 2 = 1*2 = 2
+    # # # factorial of 3 = 1*2*3 = 6
+    # # # factorial of 4 = 1*2*3*4 = 24
+# # # # factorial of 5 = 1*2*3*4*5 = 120
+print(math.factorial(n))
+
+print(math.sqrt(n)) 
+# # sqrt = square root = number which when multiplied by itself gives the original number    
+
+
+    
