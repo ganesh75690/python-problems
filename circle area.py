7dc8b5cb769609676fe84a45d31f01a028cc41a7
B SAI GANESH <149352630+ganesh75690@users.noreply.github.com>
2025-04-30 19:49:15 +0530

    Add files via upload

---
+++ circle area.py
@@ -0,0 +1,14 @@
+
+#program to find area of a circle using function
+
+def area_of_circle(r):
+    # Area of Circle = π * r^2
+    pi = 3.14
+    return pi * (r ** 2)
+
+r = 10
+
+print("area is", area_of_circle(r))          #returns the area of circle       
+
+
+  
