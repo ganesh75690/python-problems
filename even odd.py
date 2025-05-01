55275098c08f92c93af3a1c0400d3d799957c1db
B SAI GANESH <149352630+ganesh75690@users.noreply.github.com>
2025-05-01 16:07:13 +0530

    Add files via upload

---
+++ even odd.py
@@ -0,0 +1,15 @@
+# program to check if a number is even or odd
+
+def even_odd(n):
+    if n % 2 == 0:    
+        return True
+    else: 
+        return False
+
+n = 21
+print(even_odd(n)) 
+# output: True               
+
+
+
+
