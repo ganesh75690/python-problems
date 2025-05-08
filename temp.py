a89c5063a63ac2069f6f2ded8fd5f4e45e304a8e
B SAI GANESH <149352630+ganesh75690@users.noreply.github.com>
2025-05-08 13:11:12 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,19 @@
+# python program to convert celsius to fahrenheit 
+# using a function 
+
+def celcius_to_fahrenheit(celsius):
+    # formula to convert celsius to fahrenheit
+    fahrenheit = (celsius * 9/5) + 32 
+    
+    return fahrenheit
+    
+    fahranheit=(celsius*9/5)+32
+    
+    return fahranheit
+
+celsius = 30.8 
+
+fahranheit = celcius_to_fahrenheit(celsius)
+
+
+print("celsius to fahrenheit: ",celsius, "to",fahranheit)      
