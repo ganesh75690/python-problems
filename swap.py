2fab4889f60d5a871e117a7a1e2f455d23b5b0c9
B SAI GANESH <149352630+ganesh75690@users.noreply.github.com>
2025-05-08 11:46:19 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,22 @@
+# program to swap the values of two variables
+# using a temporary variable 
+
+x = 23
+
+y = 15
+
+# print the values before swapping
+
+print("before swapping: ",x, y)    
+
+# swap the values
+
+temp=x
+
+x = y
+
+y = temp
+
+# print the values after swapping
+
+print("after x and y values are : ", x,y)        
